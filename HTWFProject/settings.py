87e554e67d1011b022445b33318305efbbe72a02
toa.pita <toa.pita@gmail.com>
2022-12-06 10:21:06 -0700

    fixing final settings

--- HTWFProject/settings.py
+++ HTWFProject/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'HTWFProject.urls'
@@ -128,3 +129,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-ad70.up.railway.app']
+
+DISABLE_COLLECTSTATIC = 0
